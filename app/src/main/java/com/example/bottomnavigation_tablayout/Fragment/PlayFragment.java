44bf1077343e7d0348442cd80f72e0930981f244
a984983 <57126508+a984983@users.noreply.github.com>
2020-01-11 22:06:36 +0800

    1/11

---
+++ app/src/main/java/com/example/bottomnavigation_tablayout/Fragment/PlayFragment.java
@@ -0,0 +1,83 @@
+package com.example.bottomnavigation_tablayout.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bottomnavigation_tablayout.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class PlayFragment extends Fragment {
+
+    ViewPager viewPager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_play, container, false);
+
+        final TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        viewPager = view.findViewById(R.id.viewpager);
+
+        //Fragment嵌套Fragment要用getChildFragmentManager
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager(),0);
+
+        viewPagerAdapter.addFragment(new Tool_one_Fragment(), "Page 1");
+        viewPagerAdapter.addFragment(new Tool_two_Fragment(), "Page 2");
+        viewPagerAdapter.addFragment(new Tool_three_Fragment(), "Page 3");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).select();
+
+        return view;
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment ,String title){
+            fragments.add(fragment);
+            titles.add(title);
+            notifyDataSetChanged();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigation_tablayout/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.bottomnavigation_tablayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.bottomnavigation_tablayout.Fragment.MapFragment;
+import com.example.bottomnavigation_tablayout.Fragment.PeopleFragment;
+import com.example.bottomnavigation_tablayout.Fragment.PlayFragment;
+import com.example.bottomnavigation_tablayout.Fragment.WorkFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        //將Fragment設定到FramLayout裡顯示  並預設Fragment位置在Play
+        getSupportFragmentManager().beginTransaction().replace(R.id.framLayout,new PlayFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_play:
+                            selectedFragment = new PlayFragment();
+                            break;
+                        case R.id.nav_map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case R.id.nav_people:
+                            selectedFragment = new PeopleFragment();
+                            break;
+                        case R.id.nav_heart:
+                            selectedFragment = null;
+                            startActivity(new Intent(MainActivity.this, HeartActivity.class));
+                            break;
+                        case R.id.nav_work:
+                            selectedFragment = new WorkFragment();
+                            break;
+                    }
+
+                    if (selectedFragment != null){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framLayout,selectedFragment).commit();
+                    }
+                    return true;
+                }
+            };
+}
+
